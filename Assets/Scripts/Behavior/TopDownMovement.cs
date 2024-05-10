767c2431aa34c7b7c117eb3d9ff7ee0def7ee32a
gstk0009 <gstk0009@naver.com>
2024-05-10 10:32:05 +0900

    [Feature] 캐릭터 이동
    1. 캐릭터 이동 및 마우스 위치에 따라 x축이 반전됨.

---
+++ Assets/Scripts/Behavior/TopDownMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    [SerializeField][Range(0f, 10f)] private float moveSpeed = 5f;
+    private TopDownController controller;
+    private Rigidbody2D movementRigdbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigdbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdated - 물리(Rigidbody) 관련 업데이트
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * moveSpeed;
+
+        movementRigdbody.velocity = direction;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+}
